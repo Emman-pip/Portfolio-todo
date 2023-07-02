f19a368d42c659d30333f222d9360cc6071d2986
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-07-02 14:25:32 +0800

    Add a function to create new task(now binded to buttons yet).

---
+++ src/script.js
@@ -0,0 +1,27 @@
+function newTaskElementCreation(content) {
+  const container = document.querySelector(".tasks");
+  const newTaskButton = document.createElement("div");
+  container.appendChild(newTaskButton);
+  newTaskButton.classList.add("d-flex");
+  newTaskButton.classList.add("align-items-center");
+  newTaskButton.classList.add("gap-2");
+
+  const checkbox = document.createElement("input");
+  newTaskButton.appendChild(checkbox);
+  checkbox.type = "checkbox";
+  checkbox.classList.add("checkbox");
+  checkbox.classList.add("form-check-input");
+  checkbox.classList.add("rounded-circle");
+
+  const label = document.createElement("label");
+  newTaskButton.appendChild(label);
+  label.classList.add("form-check-label");
+  label.type = "checkbox";
+  label.textContent = content;
+}
+
+function newTaskWindow() {
+  const newTaskButton = document.createElement("div");
+}
+
+newTaskElementCreation("hello");
